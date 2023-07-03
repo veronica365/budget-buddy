4b28410c1a3c5819d354f55d0b1c3f194553698b
veronica365 <naruth10@gmail.com>
2023-07-03 21:41:06 +0300

    Add devise and user session forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'landing/index'
+  get 'landing', to: 'landing#index', as: 'landing'
   root "landing#index"
+
+  devise_for :users, path: ''
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
