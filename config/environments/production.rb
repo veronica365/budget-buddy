f3c419ad7e8fd770e2ec601ddb56b36157056419
veronica365 <naruth10@gmail.com>
2023-07-03 08:18:37 +0300

    Add devise setup with views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  # default url options
+  config.action_mailer.default_url_options = { host: 'budget-buddy-v9u9.onrender.com', port: 3000 }
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
