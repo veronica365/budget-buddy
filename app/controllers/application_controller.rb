bef4daee8cadf5867eac47d1f00f38d6427e6e29
veronica365 <naruth10@gmail.com>
2023-07-04 12:46:11 +0300

    Add styles for the user register, login and forgot password forms to match the designs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  skip_before_action :authenticate_user!, if: :landing_page!
+  skip_before_action :verify_authenticity_token, if: :landing_page!
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found!
+
+  def record_not_found!
+    render 'shared/_404'
+  end
+
+  def landing_page!
+    request.fullpath.to_s.include?('landing')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'landing', to: 'landing#index', as: 'landing'
+
+  # Defines the root path route ("/")
   root "landing#index"
 
   devise_for :users, path: ''
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 end
