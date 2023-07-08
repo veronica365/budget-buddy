bb2b936e9505d6c446366aac5b1ef8316e956ab9
veronica365 <naruth10@gmail.com>
2023-07-08 12:48:22 +0300

    Fix linter issues with styles files

--- spec/views/transactions_spec.rb
+++ spec/views/transactions_spec.rb
@@ -4,7 +4,7 @@ require_relative '../test_helper'
 RSpec.describe 'Transactions', type: :request do
   before do
     visit('/')
-    user = User.create(name: 'test', email: "tests@mail.com", password: '0000000')
+    user = User.create(name: 'test', email: 'tests@mail.com', password: '0000000')
     @category = Category.create(user_id: user.id, name: 'Utilities', icon: 'https://img.icons8.com/?size=512&id=61699&format=png')
     Transaction.create(category_id: @category.id, name: 'Water', amount: 100)
     Transaction.create(category_id: @category.id, name: 'Food', amount: 400)
@@ -12,14 +12,14 @@ RSpec.describe 'Transactions', type: :request do
     login_user(user)
   end
 
-    it 'Should have the total of the transactions displayed' do
-      visit("/categories/#{@category.id}/transactions")
-      Transaction.create(category_id: @category.id, name: 'Water', amount: 100)
-      Transaction.create(category_id: @category.id, name: 'Food', amount: 400)
-      Transaction.create(category_id: @category.id, name: 'Trash', amount: 100)
-      expect(page).to have_content('Transactions')
-      expect(page).to have_content('ADD')
-      expect(page).to have_content('Water')
-      expect(page).to have_content(600)
-    end
+  it 'Should have the total of the transactions displayed' do
+    visit("/categories/#{@category.id}/transactions")
+    Transaction.create(category_id: @category.id, name: 'Water', amount: 100)
+    Transaction.create(category_id: @category.id, name: 'Food', amount: 400)
+    Transaction.create(category_id: @category.id, name: 'Trash', amount: 100)
+    expect(page).to have_content('Transactions')
+    expect(page).to have_content('ADD')
+    expect(page).to have_content('Water')
+    expect(page).to have_content(600)
+  end
 end
